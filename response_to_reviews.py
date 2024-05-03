d0353acd3f36d593f631da9b9c1b3f108bbf7cdf
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2024-05-03 03:15:14 +0300

    Migrate to xpath

--- response_to_reviews.py
+++ response_to_reviews.py
@@ -5,58 +5,82 @@ from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from mail_parser import get_email_code
 from selenium.webdriver.common.action_chains import ActionChains
+try:
+    driver = uc.Chrome()
 
-driver = uc.Chrome()
+    driver.get('https://seller.ozon.ru/app/registration/signin?auth=1')
+    driver.set_window_size(width=1080, height=1080)
+    wait = WebDriverWait(driver, 15)
+    button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="layoutPage"]/div[1]/div/div[2]/div[1]/section/div/div/div/div/div[4]/div[3]/button')))
+    button.click()
+    email_input = wait.until(EC.visibility_of_element_located((By.ID, 'email')))
+    email_input.send_keys('karimov445@yandex.ru')
+    button = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="layoutPage"]/div[1]/div/div[2]/div[1]/section/div/div/div/div/div[4]/button')))
+    button.click()
+    sleep(15)
+    code_input = driver.find_element(By.XPATH, '//*[@id="layoutPage"]/div[1]/div/div[2]/div[1]/section/div/div/div/div/div[3]/div[1]/label/div/div/input')
+    code = get_email_code()
+    code_input.send_keys(code)
 
-driver.get('https://seller.ozon.ru/app/registration/signin?auth=1')
-driver.maximize_window()
-wait = WebDriverWait(driver, 10)
-button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'ga29-a2')))
-button.click()
-email_input = wait.until(EC.visibility_of_element_located((By.ID, 'email')))
-email_input.send_keys('karimov445@yandex.ru')
-button = driver.find_element(By.CLASS_NAME, 'b239-a')
-button.click()
-sleep(15)
-code_input = driver.find_element(By.CLASS_NAME, 'd216-a')
-code = get_email_code()
-code_input.send_keys(code)
-button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'button-module_primary_JlYlU')))
-button.click()
-button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'button-module_primary_JlYlU')))
-button.click()
-link = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Отзывы")))
-link.click()
-sleep(1)
-with open('responses.txt', 'r') as file:
-    strings = file.readlines()
-    strings = [string.replace('\n', '') for string in strings ]
-result = []
-for string in strings:
-    if string:
-        result.append(string)
-link = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "tab-module_block_9cu1q")))
-link.click()
-sleep(1)
-reviews = driver.find_elements(By.CLASS_NAME, 'index_reviewTextWrapper_3SKIG')
-sleep(1)
-for i in range(len(result)):
-    reviews[i].click()
+
+    button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[value='3907528_1199077']")))
+    button.click()
+
+    button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div[1]/div/div/div[2]/div/div/div[5]/button[2]')))
+    button.click()
+
+
+    link = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Отзывы")))
+    link.click()
+
+    link = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/main/div[1]/div[1]/div/div/div[2]/div[2]/button[2]')))
+    link.click()
+
+    with open('responses.txt', 'r') as file:
+        strings = file.readlines()
+        strings = [string.replace('\n', '') for string in strings ]
+    result = []
+    for string in strings:
+        if string:
+            result.append(string)
+
+    while True:
+        sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        try:
+            show_more_button = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "index_showMoreBtn_1gCQz")))
+            show_more_button.click()
+        except:
+            break
+
     sleep(1)
-    javascript_code = f'''var textareaElement = document.querySelector('.index_textarea_1rzYd');
-        var textToInsert = '{result[i]}';
-        textareaElement.value = textToInsert;
-        var event = new Event('input',''' + ''' { bubbles: true });
-        textareaElement.dispatchEvent(event);
-        var button = document.querySelector('.custom-button_button_sTiyh');
-        button.click();
-        '''
-    driver.execute_script(javascript_code)
-    send_button = "var button = document.querySelector('.custom-button_button_sTiyh'); button.click();"
-    driver.execute_script(send_button)
-    close_btn = "var button = document.querySelector('.window-module_closeIcon_P6SGd'); button.click();"
-    driver.execute_script(close_btn)
-    driver.execute_script("window.scrollBy(0, 100);")
+    driver.execute_script("window.scrollTo(0, 200)")
+    reviews = driver.find_elements(By.CLASS_NAME, 'index_reviewTextWrapper_3SKIG')
+    sleep(5)
 
-sleep(10)
-driver.quit()
+    for i in range(len(result)):
+        review = driver.find_element(By.XPATH, f'//*[@id="app"]/main/div[1]/div[1]/div/div/div[2]/div[3]/div/div[2]/div/div/table/tbody/tr[{i + 1}]/td[6]')
+        review.click()
+        sleep(1)
+        javascript_code = f'''var textareaElement = document.querySelector('.index_textarea_1rzYd');
+            var textToInsert = '{result[i]}';
+            textareaElement.value = textToInsert;
+            var event = new Event('input',''' + ''' { bubbles: true });
+            textareaElement.dispatchEvent(event);
+            var button = document.querySelector('.custom-button_button_sTiyh');
+            button.click();
+            '''
+        driver.execute_script(javascript_code)
+        send_button = "var button = document.querySelector('.custom-button_button_sTiyh'); button.click();"
+        driver.execute_script(send_button)
+        close_btn = "var button = document.querySelector('.window-module_closeIcon_P6SGd'); button.click();"
+        driver.execute_script(close_btn)
+        driver.execute_script("window.scrollBy(0, 100);")
+
+    sleep(10)
+except:
+    print("Too many request, or cloudfare alert, please try again")
+    exit()
+
+finally:
+    driver.quit()
--- review_scraper.py
+++ review_scraper.py
@@ -4,28 +4,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from mail_parser import get_email_code
-import sys
-
-driver = uc.Chrome()
 
 try:
+    driver = uc.Chrome()
+
     driver.get('https://seller.ozon.ru/app/registration/signin?auth=1')
     driver.set_window_size(width=1080, height=1080)
     wait = WebDriverWait(driver, 15)
-    try:
-        button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[1]/div/div[2]/div[1]/section/div/div/div/div/div[4]/div[4]/button')))
-        button.click()
-    except Exception as e:
-        print(f"Error encountered: {str(e)}")
-        driver.quit()
-        sys.exit("Too many requests, or Cloudflare alert, please try again later.")
-
+    button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="layoutPage"]/div[1]/div/div[2]/div[1]/section/div/div/div/div/div[4]/div[3]/button')))
+    button.click()
     email_input = wait.until(EC.visibility_of_element_located((By.ID, 'email')))
     email_input.send_keys('karimov445@yandex.ru')
-    button = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'b210-a')))
+    button = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="layoutPage"]/div[1]/div/div[2]/div[1]/section/div/div/div/div/div[4]/button')))
     button.click()
     sleep(15)
-    code_input = driver.find_element(By.CLASS_NAME, 'd00-a')
+    code_input = driver.find_element(By.XPATH, '//*[@id="layoutPage"]/div[1]/div/div[2]/div[1]/section/div/div/div/div/div[3]/div[1]/label/div/div/input')
     code = get_email_code()
     code_input.send_keys(code)
 
@@ -33,14 +26,14 @@ try:
     button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[value='3907528_1199077']")))
     button.click()
 
-    button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'button-module_primary_JlYlU')))
+    button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div[1]/div/div/div[2]/div/div/div[5]/button[2]')))
     button.click()
 
 
     link = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Отзывы")))
     link.click()
 
-    link = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "tab-module_block_9cu1q")))
+    link = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/main/div[1]/div[1]/div/div/div[2]/div[2]/button[2]')))
     link.click()
 
     sleep(1)
@@ -48,18 +41,22 @@ try:
         sleep(1)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
         try:
-            show_more_button = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "index_showMoreBtn_1gCQz")))
+            show_more_button = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/main/div[1]/div[1]/div/div/div[2]/div[3]/div/div[2]/div/button')))
             show_more_button.click()
         except:
             break
 
     sleep(2)
-    comments = driver.find_elements(By.CLASS_NAME, 'index_review_MIfmk')
-    grades = driver.find_elements(By.CLASS_NAME, 'index_ratingWrapper_1p0pa')
+    num_of_reviews = int(driver.find_element(By.XPATH, '//*[@id="app"]/main/div[1]/div[1]/div/div/div[2]/div[2]/button[2]/div/div[2]/div').text)
     with open('reviews.txt', 'w') as file:
-        for i in range(len(comments)):
-            comment_text = f"{comments[i].text} Оценка: {grades[i].text}"
+        for i in range(num_of_reviews):
+            grade = driver.find_element(By.XPATH, f'//*[@id="app"]/main/div[1]/div[1]/div/div/div[2]/div[3]/div/div[2]/div/div/table/tbody/tr[{i + 1}]/td[7]').text
+            review = driver.find_element(By.XPATH, f'//*[@id="app"]/main/div[1]/div[1]/div/div/div[2]/div[3]/div/div[2]/div/div/table/tbody/tr[{i + 1}]/td[6]').text
+            comment_text = f"{review} Оценка: {grade}"
             file.write(comment_text + '\n***\n')
+except:
+    print("Too many request, or cloudfare alert, please try again")
+    exit()
+    
 finally:
     driver.quit()
-    print("Driver has been terminated")
